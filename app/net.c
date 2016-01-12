6f475d98808e7e66b7e73c05fc8297020daa4cdc
qingqingcaoyuanlin <1451368913@qq.com>
2016-01-12 17:21:21 +0800

    添加了与网络的相关部分，记得malloc后要free

--- app/net.c
+++ app/net.c
@@ -13,12 +13,8 @@ const int Port[sock_All] = {
 	PORT_1,
 	PORT_2
 };
-
 int Net_Function_Init()
 {
-	struct sockaddr_in netsock;
-	int on = 1;
-	int Buff = SockBuffLen;
 	int i = 0;
 	for(i=0; i<sock_All; i++)
 	{
@@ -28,20 +24,71 @@ int Net_Function_Init()
 		{
 			printf("creat socket error\n");
 			return 0
-		}
-		netsock.sin_family = AF_INET;
-		netsock.sin_port = htons(Port[i]);
-		netsock.sin_addr.s_addr = htonl(INADDR_ANY);
-
-		if(bind(Sock_Fd[i], (struct sockaddr_in *)&netsock, sizeof(struct sockaddr_in)) < 0)
-		{
-			printf("bind fail\n");
-			return 0
-		}
-		
-	}	
-	Set_Socket_Attribute();
+		} 		
+	}	 
 	return 1;
+}
+void Net_Init()
+{
+	
+}
+int Net_Set(int *Fd, struct hostent * Host_addr, int Port, char * Msg,int Size)
+{ 
+	struct hostent * host_Addr = (struct hostent *)Host_addr;
+	struct sockaddr_in server_addr;
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(Port);
+	server_addr.sin_addr = *(host_Addr->h_addr);
+	
+	if(connect(*Fd, &server_addr, sizeof(server_addr)) == -1)
+	{
+		printf("connect error\n");
+		free(host_Addr);
+		return -1;
+	}
+	if(send(*Fd, Msg , Size, 0)== -1)
+	{
+		printf("send error\n");
+		free(host_Addr);
+		return -2;
+	}
+	free(host_Addr);
+	return 1;
+}
+void Net_Send_Msg(struct hostent * Host_addr, int Port, char * Msg, int Size)
+{
+	int fb;
+	fb = socket(AF_INET, SOCK_STREAM, 0);
+	Net_Set(&fb, Host_addr, Port, Msg, Size);
+}
+struct hostent * Get_Server_By_Addr(char * Ip)
+{
+	struct in_addr *ip_addr; 
+	struct hostent *host;
+	host = malloc(sizeof(struct hostent));		//这个要记得进行free
+	char *ip = Ip;
+	if(!inet_aton(ip, ip_addr))
+	{
+		printf("inet error\n");
+	}
+	if((host = gethostbyaddr(ip_addr, 4, AF_INET)) == NULL)
+	{
+		printf("Get Host Addr Error\n");
+	}
+	return host;
+}
+struct hostent * Get_Server_By_Name(char * Name)
+{
+	struct hostent * host;
+	char * name = Name;
+	if((host = gethostbyname(name)) == NULL)
+	{
+		printf("Get Host Name Error\n");
+	}
+	printf("hostname : %s\n",host->h_name);
+	return host;
 }
 void Set_Socket_Attribute()
 {
@@ -49,5 +96,5 @@ void Set_Socket_Attribute()
 }
 void Socket_Process()
 {
- 
+	
 }
